589b5142977836aac69360b7948183762e9bee4e
Jia <jiavon@ualberta.ca>
2020-08-12 17:02:20 -0600

    is storing inputs from user, however only one at a time

--- add_to_do.php
+++ add_to_do.php
@@ -6,18 +6,22 @@ include 'templates/header.php';
 ?>
 <!--End of the template header being used-->
 
+<!--Storing Active To-Dos in a list and into an array-->
+<?php $activeToDoList = array(); ?>
+
+
 <!--This file only take inputs from user and output to storage-->
 <form action="add_to_do.php" method="POST">
     <label for="add_to_do">
         Enter a new task: 
         <input type="text" id="add_to_do" name="add_to_do">
+        <?php array_push($activeToDoList, $_POST["add_to_do"])?>
     </label>
     <input type="submit" value="Add Task">
     <input type="reset"  value="Reset To-Do List">
 </form>
 
 <h2>Parsley's Active To-Dos</h2>
-
 
     <input type="checkbox" id="todo" name="todo">
     <label>[to be added with stored data]</label>
@@ -26,6 +30,6 @@ include 'templates/header.php';
 <h2>Debugger</h2>
 <?php
     echo '<pre>';
-    var_dump($_POST);
+    var_dump($activeToDoList);
     echo '</pre>'
 ?>
